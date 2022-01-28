715f821a699c7030559ec967fdfd1e4266658f33
Junior Torres <joetorres@blackbook.attlocal.net>
2022-01-28 13:59:44 -0800

    Added Footer.

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,28 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Image, VStack, Heading, Link } from '@chakra-ui/react';
+
 import React from 'react';
+import Socials from './Socials';
+const logoImg = "../logo.png"
+
 
 const Footer = () => {
-  return <Box>
-      Footer
-  </Box>;
+  return(
+      <Flex
+    p="1rem"
+    className="color-change-2x"          
+
+      justifyContent={"space-between"}
+      flexDirection={"row"}
+      background="white"
+      textColor={"black"}
+      alignItems={"center"}
+    >
+<Box></Box>
+<VStack><Heading fontFamily={"BodoAmat"}>Contract Address</Heading><Link href={"https://polygonscan.com/"} is External fontFamily={"BodoAmat"}>Coming Soon</Link></VStack>
+        <Box></Box>
+        </Flex>
+
+  );
 };
 
 export default Footer;
